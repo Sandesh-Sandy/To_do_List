888495579039b52de4eba5678a07534ec9f19f0f
Sandesh Kumar K S <sandeshkumar555dreams@gmail.com>
2021-05-08 22:33:16 +0530

    Updates

---
+++ precache-manifest.761452849b00a32e25d8d5e4ed619711.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4b50defc7b510f86f54d069440a82e",
+    "url": "/Sandesh-Sandy/To_do_List/index.html"
+  },
+  {
+    "revision": "4fdd1e87f021955ae2da",
+    "url": "/Sandesh-Sandy/To_do_List/static/css/main.46b4d85f.chunk.css"
+  },
+  {
+    "revision": "68ff4e0575852f416dab",
+    "url": "/Sandesh-Sandy/To_do_List/static/js/2.82e2241c.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/Sandesh-Sandy/To_do_List/static/js/2.82e2241c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fdd1e87f021955ae2da",
+    "url": "/Sandesh-Sandy/To_do_List/static/js/main.ebc261d7.chunk.js"
+  },
+  {
+    "revision": "02d29d2fb1fce4984767",
+    "url": "/Sandesh-Sandy/To_do_List/static/js/runtime-main.0d53aae8.js"
+  },
+  {
+    "revision": "04fabb58d62639e108f1849a240949cc",
+    "url": "/Sandesh-Sandy/To_do_List/static/media/bg.04fabb58.webp"
+  }
+]);
